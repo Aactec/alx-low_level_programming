50e6b53a753d92b6df9b0e1cb4c54e6a9161a906
Aactec <aacrajitech2024@gmail.com>
2023-08-11 00:49:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main - print alphabets in lower case a-z
+ *
+ */
+int main(void)
+{
+	char i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
